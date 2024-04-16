963dc64b373a8cd369d174f563bc7c7d8df751b5
unknown <arusuvinam@gmail.com>
2024-04-16 14:46:37 +0530

    jj

---
+++ src/org/ccom/Comany.java
@@ -0,0 +1,15 @@
+package org.ccom;
+
+public class Comany {
+	private void companyName() {
+		System.out.println("ibm");
+
+	}
+	public static void main(String[] args) {
+		
+		Comany c= new Comany();
+		c.companyName();
+		
+	}
+
+}
